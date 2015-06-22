bd66e662ac38bb111bc03ef7d6371c94ad5350ef
Tezika <zfk565614653@outlook.com>
2015-06-22 21:27:19 +0800

    add file I/O

--- Reflection_2/main.cpp
+++ Reflection_2/main.cpp
@@ -7,13 +7,32 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include "WeaponHeader.h"
+
+
+const char* FILE_PATH = "/Users/Tezika/Projects/CodingLearn/Reflection_2/Reflection_2/Test.txt";
+void readCofigFile();
+void disWeaponInfo(string str);
+
+void readCofigFile(){
+    ifstream ifs(FILE_PATH,ios::in);
+    string str_perLine;
+    while (!ifs.eof()) {
+        getline(ifs, str_perLine);
+        disWeaponInfo(str_perLine);
+    }
+    ifs.close();
+}
+
+void disWeaponInfo(string str){
+    
+}
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     //std::cout << "Hello, World!\n";
-    auto  pTst = Object::createObject("Bazooka");
-    pTst->disWeaponName();
-   // pTst->getId();
-    delete pTst;
+    readCofigFile();
     return 0;
 }
