209f1d2eed2d15a2be566a1d26ecaad9a125dff5
Tezika <zfk565614653@outlook.com>
2015-06-28 20:14:39 +0800

    modify config txt

---
+++ dylib_reflectorclasses/dylib_reflectorclasses/OtherGun.cpp
@@ -0,0 +1,9 @@
+//
+//  OtherGun.cpp
+//  dylib_reflectorclasses
+//
+//  Created by TonyZefkia on 6/28/15.
+//  Copyright (c) 2015 TonyZefkia. All rights reserved.
+//
+
+#include "OtherGun.h"
---
+++ dylib_reflectorclasses/dylib_reflectorclasses/OtherGun.h
@@ -0,0 +1,14 @@
+//
+//  OtherGun.h
+//  dylib_reflectorclasses
+//
+//  Created by TonyZefkia on 6/28/15.
+//  Copyright (c) 2015 TonyZefkia. All rights reserved.
+//
+
+#ifndef __dylib_reflectorclasses__OtherGun__
+#define __dylib_reflectorclasses__OtherGun__
+
+#include <stdio.h>
+
+#endif /* defined(__dylib_reflectorclasses__OtherGun__) */
